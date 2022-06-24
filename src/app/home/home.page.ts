45830a82107239bb4a2f2cfd910eb84da25af238
Victor Oliveira <victor.oliveira@blueservice.com.br>
2022-06-24 15:39:16 -0300

    selecionando categoria

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,113 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit ,Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { IonContent, IonList } from '@ionic/angular';
+import {tap,switchMap,map} from 'rxjs/operators';
+
+export interface Category {
+    id:number,
+    name: string;
+    detail_image:string;
+    title: string;
+    subtitle: string;
+}
+
+export interface Product {
+  id:number;
+  categories: number;
+  name: string;
+  thumbnail_image: string;
+  detail_image: string;
+  description: string;
+  price: number;
+}
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit,AfterViewInit{
+ url = 'https://academy-coffee-shop-api.herokuapp.com/';
+ categories: Category[] = [];
+ products = [];
+  opts = {
+  freeMode: true,
+  slidesPerView: 2.8,
+  slidesOffsetBefore:30,
+  slidesOffsetAfter: 100
+  }
 
-  constructor() {}
+  activeCategory = 0;
+  cateforySlidesVisible = true;
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.http.get<Category[]>(`${this.url}/categories`).pipe(
+      tap(categories => {
+        for(let c of categories){
+          c.detail_image = `${this.url}${c.detail_image}`;
+        }
+        this.categories = categories;
+      }),
+      switchMap(res => {
+        console.log(res, ' swithmap');
+
+        return this.http.get<Product[]>(`${this.url}/products`);
+      }),
+      map (products => {
+        for(let product of products){
+          product.thumbnail_image = `${this.url}${product.thumbnail_image}`;
+        }
+        return products;
+      }),
+      map(results => {
+        const productMap = {};
+        for(let product of results){
+          if(!productMap[product.categories]){
+            productMap[product.categories] = [product];
+          } else {
+            productMap[product.categories].push(product);
+          }
+        }
+        const orderedProducts = [];
+
+        Object.entries(productMap).forEach(([k,v]) => {
+          const category = this.getCategory(+k);
+
+          const entry = {
+            ...category,
+            products: v
+          }
+          orderedProducts.push(entry);
+        });
+        return orderedProducts;
+      })
+    )
+    .subscribe(results => {
+      this.products = results;
+      // console.log(results);
+    });
+  }
+
+  @ViewChildren(IonList, { read: ElementRef }) lists: QueryList<ElementRef>;
+  listElements = [];
+  @ViewChild(IonContent) content: IonContent;
+
+  getCategory(catedoryId: number){
+    return this.categories.filter(c => c.id === catedoryId)[0];
+  }
+
+
+  ngAfterViewInit(){
+    this.lists.changes.subscribe(_ => {
+      this.listElements = this.lists.toArray();
+      console.log(this.listElements, 'listelements')
+    })
+  }
+
+  selectCategory(index){
+    const child = this.listElements[index].nativeElement;
+    this.content.scrollToPoint(0, child.offsetTop - 100, 1000)
+  }
 }
